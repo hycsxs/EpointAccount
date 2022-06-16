6342b51b46542aa74ad63e187236abb8ee1afb1e
huangyc <hycsxs@qq.com>
2022-06-16 10:14:52 +0800

    111

--- pages/account/js/account_proof.js
+++ pages/account/js/account_proof.js
@@ -18,10 +18,30 @@ var voucherlist = [];//所有凭证
 (function(doc, Config, Util) {
 'use strict';
 
+Date.prototype.Format = function(fmt) {
+    var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "H+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt))
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+    return fmt;
+}
+
+
 Util.loadJs([
     'pages/common/common.js',
     'pages/common/libs/swiper.min.js',
-    'pages/common/libs/swiper.min.css'
+    'pages/common/libs/swiper.min.css',
+    'pages/common/util.date_extend.js'
    //  'pages/account/account_canteen_util.js',
 
 ], 
@@ -90,10 +110,10 @@ function getCanteenVoucher(){
             {
               "canteenname": "总部食堂(东区)",
               "vouchertype": "lunch",
-              "createdate": new Date().Format("yyyy-MM-dd hh:mm:ss"),
-              "endtime": "2022-06-15 13:30:00",
+              "createdate": new Date().Format("yyyy-MM-dd 11:30:00"),
+              "endtime": new Date().Format("yyyy-MM-dd 13:00:00"),
               "operateusername": "黄颖超",
-              "personno": "20150704088",
+              "personno": "20210702278",
               "dinernum": 1
             }
           ]
